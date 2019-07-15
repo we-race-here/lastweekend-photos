54b2a0f80a1d61250c8a5a26f84dcbd9d9a9b1df
pprolancer <pprolancer@gmail.com>
2019-07-14 23:47:45 -0700

    - build ui

--- wrh_photos/FRONTEND/wrh_photos_ui/src/components/partials/MainFooter.vue
+++ wrh_photos/FRONTEND/wrh_photos_ui/src/components/partials/MainFooter.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="kt-footer kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop">
-    <div class="kt-footer__copyright">
-      2019&nbsp;&copy;&nbsp;<a href="https://www.weracehere.org/" target="_blank" class="kt-link">We-Race-Here.&nbsp;</a>
-      Developed by&nbsp;<a target="_blank" href="http://www.heteroskedastic.com/" class="kt-link"> Heteroskedastic</a>
-    </div>
-    <div class="kt-footer__menu">
+  <div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
+    <div class="kt-container  kt-container--fluid ">
+      <div class="kt-footer__copyright">
+        2019&nbsp;&copy;&nbsp;<a href="https://www.weracehere.org/" target="_blank" class="kt-link">We-Race-Here.&nbsp;</a>
+      </div>
+      <div class="kt-footer__menu">
       <a href="javascript:" target="_blank" class="kt-footer__menu-link kt-link">About</a>
       <a href="javascript:" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
       <a href="javascript:" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
+      </div>
     </div>
   </div>
 </template>
